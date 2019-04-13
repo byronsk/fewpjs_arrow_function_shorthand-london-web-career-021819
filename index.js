ed2637df1b4c5f90e679ab8878dea821d7fd16bd
byronsk <vyskoutaris@hotmail.com>
2019-04-13 14:35:16 +0100

    Done

---
+++ index.js
@@ -0,0 +1,16 @@
+
+//Functional Expression
+let divide = function() {
+  return 2000/100;
+}
+
+divide()
+
+//Arrow Function
+let square = (n1) => n1 * n1
+
+square(2)
+
+let add = (n1, n2) => n1 + n2
+
+add(2,3)
